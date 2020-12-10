0d25c8082bee3165ff11b90909b78b90c301abce
LCW <2733064152@qq.com>
2020-12-10 15:13:25 +0800

    今日制作用户列表功能

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class Main {
+
+    @Test
+    public void testHelloWrold() {
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+        System.out.println("HelloWorld!");
+    }
+
+}
